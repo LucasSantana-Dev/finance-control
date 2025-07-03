fbb20499695bab40f78d8cc376f32baaba58059d
LucasSantana-Dev <lucas.diassantana@gmail.com>
2025-07-03 02:00:38 -0300

    chore(config): update environment and application configuration files

--- src/main/java/com/finance_control/FinanceControlApplication.java
+++ src/main/java/com/finance_control/FinanceControlApplication.java
@@ -2,10 +2,8 @@ package com.finance_control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class FinanceControlApplication {
 
 	public static void main(String[] args) {
